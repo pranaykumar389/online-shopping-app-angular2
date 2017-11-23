1bf45d7c29ba625573d1d60f15334c38cf69ec0e
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2017-11-23 12:15:18 -0500

    added a logout button

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -23,6 +23,11 @@ export class AuthService {
             )
     }
 
+    logout() {
+        firebase.auth().signOut();
+        this.token = null;
+    }
+
     getToken() {
         firebase.auth().currentUser.getIdToken()
             .then(
