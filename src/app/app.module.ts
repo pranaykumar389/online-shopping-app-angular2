de1a97c55bde950dc35c111af52a087ab4564179
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2017-11-23 02:18:51 -0500

    setup firebase as a backend database and sendong PUT requests to save data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,8 @@ import { AppRoutingModule } from 'app/app-routing.module';
 import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { RecipeService } from 'app/recipes/recipe.service';
+import { HttpModule } from '@angular/http';
+import { DataStorageService } from 'app/shared/data-storage.service';
 
 @NgModule({
   declarations: [
@@ -35,9 +37,10 @@ import { RecipeService } from 'app/recipes/recipe.service';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpModule
   ],
-  providers: [ShoppingListService, RecipeService],
+  providers: [ShoppingListService, RecipeService, DataStorageService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,8 +1,22 @@
 import { Component } from '@angular/core';
+import { Response } from '@angular/http';
+
+import { DataStorageService } from 'app/shared/data-storage.service';
 
 @Component ({
     selector: 'app-header',
     templateUrl: './header.component.html',
     styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+
+    constructor(private dataStorageService: DataStorageService){}
+
+    onSaveData() {
+        this.dataStorageService.storeRecipes()
+            .subscribe(
+                (response: Response) => console.log(response),
+                (error) => console.log(error)
+            );
+    }
+}
