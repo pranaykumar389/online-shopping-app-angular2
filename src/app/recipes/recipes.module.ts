3ee44859d1038ca874fda6df176e6ab5f3a4e2c1
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2017-11-23 22:39:53 -0500

    created a Recipe Feature Module

---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from "@angular/core";
+import { ReactiveFormsModule } from "@angular/forms";
+import { CommonModule } from "@angular/common";
+
+import { RecipesComponent } from "app/recipes/recipes.component";
+import { RecipeStartComponent } from "app/recipes/recipe-start/recipe-start.component";
+import { RecipeListComponent } from "app/recipes/recipe-list/recipe-list.component";
+import { RecipeEditComponent } from "app/recipes/recipe-edit/recipe-edit.component";
+import { RecipeDetailComponent } from "app/recipes/recipe-detail/recipe-detail.component";
+import { RecipeItemComponent } from "app/recipes/recipe-list/recipe-item/recipe-item.component";
+
+@NgModule({
+    declarations: [
+        RecipesComponent,
+        RecipeStartComponent,
+        RecipeListComponent,
+        RecipeEditComponent,
+        RecipeDetailComponent,
+        RecipeItemComponent
+    ],
+    imports: [
+        ReactiveFormsModule,
+        CommonModule
+    ]
+})
+export class RecipesModule {}
