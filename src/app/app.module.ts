82ff2e25693124be60543dc2dc85f072927aae5b
Pranay Kumar Rudra <rudrapranaykumar@gmail.com>
2017-11-25 00:15:36 -0500

    Restructured Services to use the Child injector

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,8 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { ShoppingListService } from 'app/shopping-list/shopping-list.service';
 import { AppRoutingModule } from 'app/app-routing.module';
-import { RecipeService } from 'app/recipes/recipe.service';
 import { HttpModule } from '@angular/http';
-import { DataStorageService } from 'app/shared/data-storage.service';
-import { AuthService } from 'app/auth/auth.service';
-import { AuthGuard } from 'app/auth/auth-guard.service';
 import { SharedModule } from 'app/shared/shared.module';
 import { ShoppingListModule } from 'app/shopping-list/shopping-list.module';
 import { AuthModule } from 'app/auth/auth.module';
@@ -27,7 +22,7 @@ import { CoreModule } from 'app/core/core.module';
     AuthModule,
     CoreModule
   ],
-  providers: [ShoppingListService, RecipeService, DataStorageService, AuthService, AuthGuard],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -4,6 +4,11 @@ import { HeaderComponent } from "app/core/header/header.component";
 import { HomeComponent } from "app/core/home/home.component";
 import { SharedModule } from "app/shared/shared.module";
 import { AppRoutingModule } from "app/app-routing.module";
+import { ShoppingListService } from "app/shopping-list/shopping-list.service";
+import { RecipeService } from "app/recipes/recipe.service";
+import { DataStorageService } from "app/shared/data-storage.service";
+import { AuthService } from "app/auth/auth.service";
+import { AuthGuard } from "app/auth/auth-guard.service";
 
 @NgModule({
     declarations: [
@@ -17,6 +22,13 @@ import { AppRoutingModule } from "app/app-routing.module";
     exports: [
         AppRoutingModule,
         HeaderComponent
+    ],
+    providers: [
+        ShoppingListService, 
+        RecipeService, 
+        DataStorageService, 
+        AuthService, 
+        AuthGuard
     ]
 })
 export class CoreModule {}
